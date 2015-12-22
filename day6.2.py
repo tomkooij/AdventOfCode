a3c288e6f3881c3c5a94fd046e0ec2fc373b4daf
tomkooij <tomkooij@tomkooij.nl>
2015-12-22 15:50:31 +0100

    pass function toggle, on, off

--- day6.2.py
+++ day6.2.py
@@ -1,5 +1,5 @@
 #adventofcode.com
-#day6.1
+#day6.2
 
 INPUTFILE = 'input/input6'
 
@@ -7,25 +7,15 @@ SIZE = 1000
 
 lights = SIZE*SIZE*[0]
 
-def toggle(x1,y1,x2,y2):
-    #print "toggle: ", x1, y1, x2, y2
-    for x in range(x1,x2+1):
-        for y in range(y1,y2+1):
-            lights[SIZE*x+y] += 2
+def toggle(x): return x + 2
+def on(x): return x + 1
+def off(x): return max(x - 1, 0)
 
-def on(x1,y1,x2,y2):
-    #print "on: ", x1, y1, x2, y2
-    for x in range(x1,x2+1):
-        for y in range(y1,y2+1):
-            lights[SIZE*x+y] += 1
+def do(f, x1, y1, x2, y2):
 
-def off(x1,y1,x2,y2):
-    #print "off: ", x1, y1, x2, y2
     for x in range(x1,x2+1):
         for y in range(y1,y2+1):
-            lights[SIZE*x+y] -= 1
-            if lights[SIZE*x+y] < 0:
-                lights[SIZE*x+y] = 0
+            lights[SIZE*x+y] = f(lights[SIZE*x+y])
 
 
 with open(INPUTFILE) as f:
@@ -36,14 +26,14 @@ with open(INPUTFILE) as f:
         if s[0] == 'toggle':
             x1,y1 = s[1].split(',')
             x2,y2 = s[3].split(',')
-            toggle(int(x1),int(y1),int(x2),int(y2))
+            do(toggle, int(x1),int(y1),int(x2),int(y2))
         if s[0] == 'turn' and s[1] == 'on':
             x1,y1 = s[2].split(',')
             x2,y2 = s[4].split(',')
-            on(int(x1),int(y1),int(x2),int(y2))
+            do(on, int(x1),int(y1),int(x2),int(y2))
         if s[0] == 'turn' and s[1] == 'off':
             x1,y1 = s[2].split(',')
             x2,y2 = s[4].split(',')
-            off(int(x1),int(y1),int(x2),int(y2))
+            do(off, int(x1),int(y1),int(x2),int(y2))
 
     print "brightness: ", sum(lights)
