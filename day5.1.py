185f88b8fcbb111160bfb32c8506aba6ef82aa23
tomkooij <tomkooij@tomkooij.nl>
2015-12-22 14:53:24 +0100

    solution to day5 part 1

---
+++ day5.1.py
@@ -0,0 +1,46 @@
+#adventofcode.com
+#day5.1
+
+INPUTFILE = 'input/input5'
+
+VOWELS = 'aeiou'
+FORBIDDEN = ['ab', 'cd', 'pq', 'xy']
+
+TESTCASES = ['ugknbfddgicrmopn', 'aaa', 'jchzalrnumimnmhp', 'haegwjzuvuyypxyu', 'dvszwmarrgswjxmb']
+
+
+def is_nice(string):
+
+    # at least 3 vowels:
+    number_of_vowels = 0
+    for v in VOWELS:
+        number_of_vowels += string.count(v)
+    if number_of_vowels < 3:
+        return False
+
+    doubles = False
+
+    for i in range(len(string)-1):
+        if string[i]==string[i+1]:
+            doubles = True
+    if not doubles:
+        return False
+
+    for illegal in FORBIDDEN:
+        if string.find(illegal) != -1:
+            return False
+
+    return True
+
+
+for testcase in TESTCASES:
+    print testcase, is_nice(testcase)
+
+with open(INPUTFILE) as f:
+    lines = f.readlines()
+
+    count = 0
+    for line in lines:
+        if is_nice(line):
+            count += 1
+    print "%d nice strings in %s" % (count, INPUTFILE)
