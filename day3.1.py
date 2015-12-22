42d2e6b3cfc0d345e6172702b1993ca35a2b5020
tomkooij <tomkooij@tomkooij.nl>
2015-12-22 11:26:20 +0100

    solution for day3.1

---
+++ day3.1.py
@@ -0,0 +1,47 @@
+# adventofcode.com
+# day 3.1
+
+import itertools
+
+INPUTFILE = 'input/input3'
+
+TESTCASE1 = '>'
+TESTCASE2 = '^>v<'
+TESTCASE3 = '^v^v^v^v^v'
+
+total = 0
+
+UP = '^'
+DOWN = 'v'
+LEFT = '<'
+RIGHT = '>'
+
+x,y = 0,0
+trip = []  # list of locations
+
+with open(INPUTFILE) as f:
+    inputstr = f.readline()
+    #inputstr = TESTCASE2
+
+    # start position
+    trip.append((x,y))
+
+    for char in inputstr:
+
+        if char == UP:
+            y += 1
+        elif char == DOWN:
+            y -= 1
+        elif char == LEFT:
+            x -= 1
+        elif char == RIGHT:
+            x += 1
+        else:
+            print "skipping unkown input: ", char
+
+        trip.append((x,y))
+
+    # remove duplicates
+    trip = set(trip)
+
+    print "Santa visited %d unique locations" % len(trip)
