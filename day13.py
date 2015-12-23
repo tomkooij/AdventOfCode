2c4b96eadef277b28960d582d7e47b2452dab9a9
tomkooij <tomkooij@tomkooij.nl>
2015-12-23 21:37:50 +0100

    testcase successful

--- day13.py
+++ day13.py
@@ -1,16 +1,30 @@
 # adventofcode
 # day 13
 
+from collections import defaultdict
+
 INPUTFILE = 'input/input13'
+TESTINPUTFILE = 'input/test13'
+TESTCASE = ['David', 'Alice', 'Bob', 'Carol']
 
 
+def happiness(seats, potential):
+
+    total = 0
+    for i in range(len(seats)):
+        left = i
+        right = i+1 if i+1 < len(seats) else 0
+        total += potential[seats[left]][seats[right]]
+        total += potential[seats[right]][seats[left]]
+    return total
 
 
 def parse(infile):
     with open(infile) as f:
         lines = f.readlines()
 
-        potential = []
+        names = []
+        potential = defaultdict(dict)
 
         for line in lines:
             s = line.split()
@@ -21,10 +35,15 @@ def parse(infile):
                 gain = -gain
             name2 = s[10][:-1]  # remove last .
 
-            potential.append((name1, gain, name2))
+            potential[name1][name2] = gain
+            names.append(name1)
 
-    return potential
+    return list(set(names)), potential
 
 
 if __name__ == '__main__':
-    potential = parse(INPUTFILE)
+    names, testpotential = parse(TESTINPUTFILE)
+    names = TESTCASE
+    score = happiness(TESTCASE, testpotential)
+    print names, score
+    assert score == 330, 'testcase fails!'
