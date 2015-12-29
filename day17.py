7d49c2790eb033d8600fcd4dd7c4de8341d2fd61
tomkooij <tomkooij@tomkooij.nl>
2015-12-29 08:26:24 +0100

    added part B

--- day17.py
+++ day17.py
@@ -8,10 +8,14 @@ INPUT = 'input/input17'
 with open(INPUT) as f:
     sizes = map(int, f.readlines())
 
-total = 0
+total, partB = 0, 0
 for size in range(len(sizes)-1):
     print "size: ", size
     for combi in combinations(sizes, size):
         if sum(combi) == 150:
             total += 1
-    print "total: ", total
+    if total > 0 and partB == 0:
+        partB = total
+
+print "part A: ", total
+print "part B: ", partB
