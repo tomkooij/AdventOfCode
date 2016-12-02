99e338f3ca918995ce0e139564df2b1d3aa4106e
Tom Kooij <tomkooij@tomkooij.nl>
2016-12-02 10:06:43 +0100

    Use dict for L R rotation

--- aoc2016/day1.py
+++ aoc2016/day1.py
@@ -2,21 +2,18 @@ from collections import deque
 
 x, y = 0, 0
 
-directions = deque([lambda x, y, n: (x, y+n),   # north
+move_funcs = deque([lambda x, y, n: (x, y+n),   # north
                     lambda x, y, n: (x+n, y),   # east
                     lambda x, y, n: (x, y-n),   # south
                     lambda x, y, n: (x-n, y)])  # west
+
+direction = {'L' : 1, 'R': -1}
 
 with open('input/input.txt') as f:
     for cmd in f.readline().split(', '):
         LR = cmd[0]
         n = int(cmd[1:])
-        #print(x, y, 'going', LR, n)
-        if LR == 'L':
-            directions.rotate(1)
-        else:
-            directions.rotate(-1)
-        #print(directions[0])
-        x, y = directions[0](x, y, n)
+        move_funcs.rotate(direction[LR])
+        x, y = move_funcs[0](x, y, n)
 
 print('finished at (%d, %d). %d blocks away.' % (x, y, abs(x)+abs(y)))
--- aoc2016/day1b.py
+++ aoc2016/day1b.py
@@ -3,10 +3,12 @@ from collections import deque
 
 x, y = 0, 0
 
-directions = deque([lambda x, y, n: (x, y+n),   # north
+move_funcs = deque([lambda x, y, n: (x, y+n),   # north
                     lambda x, y, n: (x+n, y),   # east
                     lambda x, y, n: (x, y-n),   # south
                     lambda x, y, n: (x-n, y)])  # west
+
+direction = {'L' : 1, 'R': -1}
 
 visited_locations = {(x, y)}
 
@@ -14,14 +16,9 @@ with open('input/input.txt') as f:
     for cmd in f.readline().split(', '):
         LR = cmd[0]
         n = int(cmd[1:])
-        #print(x, y, 'going', LR, n)
-        if LR == 'L':
-            directions.rotate(1)
-        else:
-            directions.rotate(-1)
-        #print(directions[0])
+        move_funcs.rotate(direction[LR])
         for _ in range(n):
-            x, y = directions[0](x, y, 1)
+            x, y = move_funcs[0](x, y, 1)
             if (x, y) in visited_locations:
                 print('second time at (%d, %d). '
                       '%d blocks away.' % (x, y, abs(x)+abs(y)))
