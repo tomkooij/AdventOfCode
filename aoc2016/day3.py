9c077ba636d82f1965ca256e09631f90e4c90687
Tom Kooij <tomkooij@tomkooij.nl>
2016-12-03 15:14:04 +0100

    Use split() instead of regex
    split() just works on the input...

--- aoc2016/day3.py
+++ aoc2016/day3.py
@@ -1,6 +1,3 @@
-import re
-
-
 def check_triangle(x, y, z):
     return x + y > z and x + z > y and y + z > x
 
@@ -8,7 +5,7 @@ def check_triangle(x, y, z):
 with open('input/input3.txt') as f:
     n = 0
     for line in f.readlines():
-        x, y, z = map(int, re.findall(r'\d+', line))
+        x, y, z = map(int, line.split())
         if check_triangle(x, y, z):
             n += 1
     print(n)
--- aoc2016/day3b.py
+++ aoc2016/day3b.py
@@ -1,6 +1,3 @@
-import re
-
-
 def check_triangle(x, y, z):
     return x + y > z and x + z > y and y + z > x
 
@@ -14,8 +11,5 @@ def find_number_of_possible_triangles(triangles):
     return n
 
 with open('input/input3.txt') as f:
-    triangles = []
-    for line in f.readlines():
-        x, y, z = map(int, re.findall(r'\d+', line))
-        triangles.append((x, y, z))
+    triangles = [map(int, line.split()) for line in f.readlines()]
     print(find_number_of_possible_triangles(triangles))
