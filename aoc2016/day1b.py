46ea7657284a0425ab5826d18e2526fdd5f8b162
Tom Kooij <tomkooij@tomkooij.nl>
2016-12-03 11:32:24 +0100

    Use function to find second visit
    Clean up code: Use function to process commands.
    Blueprint for next days

--- aoc2016/day1b.py
+++ aoc2016/day1b.py
@@ -1,28 +1,30 @@
-import sys
 from collections import deque
 
-x, y = 0, 0
 
 move_funcs = deque([lambda x, y, n: (x, y+n),   # north
                     lambda x, y, n: (x+n, y),   # east
                     lambda x, y, n: (x, y-n),   # south
                     lambda x, y, n: (x-n, y)])  # west
 
-direction = {'L' : 1, 'R': -1}
+direction = {'L': 1, 'R': -1}
 
-visited_locations = {(x, y)}
 
-with open('input/input.txt') as f:
-    for cmd in f.readline().split(', '):
+def find_second_visit(commands):
+    x, y = 0, 0
+    visited_locations = {(x, y)}
+    for cmd in commands:
         LR = cmd[0]
         n = int(cmd[1:])
         move_funcs.rotate(direction[LR])
         for _ in range(n):
             x, y = move_funcs[0](x, y, 1)
             if (x, y) in visited_locations:
-                print('second time at (%d, %d). '
-                      '%d blocks away.' % (x, y, abs(x)+abs(y)))
-                sys.exit()
+                return (x, y)
             visited_locations.add((x, y))
 
-print('finished at (%d, %d). %d blocks away.' % (x, y, abs(x)+abs(y)))
+
+with open('input/input.txt') as f:
+    commands = f.readline().split(', ')
+    x, y = find_second_visit(commands)
+    print('second time at (%d, %d). '
+          '%d blocks away.' % (x, y, abs(x)+abs(y)))
