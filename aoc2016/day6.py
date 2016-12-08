a361737e8e78eec0705f01c65af47b797eb1fdd2
Tom Kooij <tomkooij@tomkooij.nl>
2016-12-08 07:15:13 +0100

    Cleanup day 6. Add input.

--- aoc2016/day6.py
+++ aoc2016/day6.py
@@ -1,11 +1,11 @@
-passwordA, passwordB = '', ''
+msg_A, msg_B = '', ''
 
 with open('input/input6-test.txt') as f:
     lines = f.readlines()
     for column in zip(*lines):
         frequencies = sorted(list({(column.count(char), char) for char in
                                                                   column}))
-        passwordA += frequencies[-1][1]
-        passwordB += frequencies[0][1]
+        msg_A += frequencies[-1][1]
+        msg_B += frequencies[0][1]
 
-print(passwordA, passwordB)
+print(msg_A, msg_B)
