5d8d828fa6e1028ae81807a231907be96505bf17
Tom Kooij <tomkooij@tomkooij.nl>
2016-12-09 13:28:43 +0100

    Day 9 part A and B

--- aoc2016/day9.py
+++ aoc2016/day9.py
@@ -1,35 +1,28 @@
-testcases = [('ADVENT', 'ADVENT'),
-            ('A(1x5)BC', 'ABBBBBC'),
-            ('(3x3)XYZ', 'XYZXYZXYZ'),
-            ('A(2x2)BCD(2x2)EFG', 'ABCBCDEFEFG'),
-            ('(6x1)(1x3)A', '(1x3)A'),
-            ('X(8x2)(3x3)ABCY', 'X(3x3)ABC(3x3)ABCY')]
-
+partB = False
 
 def decompress(s):
 
     idx = s.find('(')
     if idx == -1:
-        return s
+        return len(s)
 
     idx_endmarker = s.find(')', idx)
     marker = s[idx+1:idx_endmarker]
     number_of_chars, n = map(int, marker.split('x'))
 
     start = idx_endmarker+1
-    end = idx_endmarker+number_of_chars+1
-    repeat = s[start: end]
-    rest = s[end:]
+    endpart = idx_endmarker+number_of_chars+1
+    nextpart = s[start: endpart]
+    rest = s[endpart:]
 
-    return s[:idx] + n*repeat + decompress(rest)
-
-
-for testcase, result in testcases:
-    print (testcase)
-    length = decompress(testcase)
-    print (length, result)
-    assert length == result
+    if partB:
+        return len(s[:idx]) + n*decompress(nextpart) + decompress(rest)
+    else:
+        return len(s[:idx]) + n*len(nextpart) + decompress(rest)
 
 
 with open('input/input9.txt') as f:
-    print(len(decompress(f.readline().rstrip('\n'))))
+    s = f.readline().rstrip('\n')
+    print('A: ', decompress(s))
+    partB = True
+    print('B: ', decompress(s))
