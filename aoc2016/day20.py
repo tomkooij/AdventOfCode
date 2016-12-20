b7ba390e6559ed988b60891adde4e2a8134d1020
Tom Kooij <tomkooij@tomkooij.nl>
2016-12-20 15:26:47 +0100

    Day 20: simplify solution
    use solve()
    calculate gap in range based on max_begin and max_end only

--- aoc2016/day20.py
+++ aoc2016/day20.py
@@ -1,29 +1,24 @@
+def solve(ranges, partA=False):
+    max_begin, max_end = 0, 0
+    total_gap = 0
+
+    for begin, end in sorted(ranges):
+
+        gap = begin - (max_end + 1)
+        if gap > 0:
+            """we have found a gap in the range"""
+            if partA:
+                return max_end + 1
+            total_gap += gap
+
+        max_begin = max(begin, max_begin)
+        max_end = max(end, max_end)
+
+    return total_gap
+
+
 with open('input\input20.txt') as f:
     ranges = [tuple(map(int, line.split('-'))) for line in f.readlines()]
-    ranges.sort()
 
-max_begin, max_end = 0, 0
-must_start_at = 0
-gap = 0
-
-breakpoints = []
-for begin, end in ranges:
-
-    if begin > must_start_at:
-        """
-        The next sorted range does not begin at the right value
-        we have found a gap in the range
-        """
-        breakpoints.append(must_start_at)
-        gap += begin - (max_end + 1)
-
-    max_begin = max(begin, max_begin)
-    max_end = max(end, max_end)
-
-    if end > max_begin:
-        """extend the range"""
-        must_start_at = max(end + 1, max_end + 1)
-
-print('part A: ', breakpoints[0])
-print('part B: ', gap)
- 
+print('part A: ', solve(ranges, partA=True))
+print('part B: ', solve(ranges))
