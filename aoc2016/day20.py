4e50c113bebd4551687124cc03c5e8c258e75e5f
Tom Kooij <tomkooij@tomkooij.nl>
2016-12-20 13:13:55 +0100

    Day 20

--- aoc2016/day20.py
+++ aoc2016/day20.py
@@ -1,22 +1,29 @@
-
-
 with open('input\input20.txt') as f:
     ranges = [tuple(map(int, line.split('-'))) for line in f.readlines()]
     ranges.sort()
 
-
 max_begin, max_end = 0, 0
 must_start_at = 0
-n = 0
-for idx, (begin, end) in enumerate(ranges):
+gap = 0
+
+breakpoints = []
+for begin, end in ranges:
+
     if begin > must_start_at:
-        print('found!' , idx, begin, end, max_begin, max_end)
-        n += begin-(max_end+1)
-    print (idx, begin, end)
+        """
+        The next sorted range does not begin at the right value
+        we have found a gap in the range
+        """
+        breakpoints.append(must_start_at)
+        gap += begin - (max_end + 1)
+
     max_begin = max(begin, max_begin)
     max_end = max(end, max_end)
 
     if end > max_begin:
-        must_start_at = max(end+1, max_end+1)
+        """extend the range"""
+        must_start_at = max(end + 1, max_end + 1)
 
-print(n)
+print('part A: ', breakpoints[0])
+print('part B: ', gap)
+ 
