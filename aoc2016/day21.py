656cb0d92cfe0685586c87f91f85025f5bb65458
Tom Kooij <tomkooij@tomkooij.nl>
2016-12-21 09:58:58 +0100

    Day 21 part B
    implemented reverse operations (if needed)

--- aoc2016/day21.py
+++ aoc2016/day21.py
@@ -28,6 +28,14 @@ def rotate_pos(l, a):
     l.rotate(n)
     return list(l)
 
+def reversed_rotate_pos(l, a):
+    mapping = {1: 1, 3: 2, 5: 3, 7: 4,
+               0: 1, 2: 6, 4: 7, 6: 0}
+    n = mapping[l.index(a)]
+    l = deque(l)
+    l.rotate(-n)
+    return list(l)
+
 def reverse_pos(l, x, y):
     """
     reverse positions X through Y means that the span of letters at indexes X through Y (including the letters at X and Y) should be reversed in order.
@@ -53,21 +61,16 @@ assert ''.join(reverse_pos(list('abcdefgh'), 4, 7)) == 'abcdhgfe'
 assert ''.join(reverse_pos(list('abcdefgh'), 0, 2)) == 'cbadefgh'
 assert ''.join(move_pos(list('abcdefgh'), 2, 0)) == 'cabdefgh'
 
+# test reverse operations
+l = list('abcdefgh')
+for idx, letter in enumerate(l):
+    tmp = rotate_pos(l, letter)
+    reversed_tmp = reversed_rotate_pos(tmp, letter)
+    #print(idx, letter, ''.join(tmp), tmp.index(letter), ''.join(reversed_tmp))
+    assert reversed_tmp == l
 
-def parse(l, line):
-    """
 
-    swap position 7 with position 6
-    move position 1 to position 4
-    move position 6 to position 5
-    rotate right 1 step
-
-    reverse positions 2 through 4
-    reverse positions 1 through 4
-    reverse positions 5 through 7
-    rotate left 2 steps
-    swap letter g with letter f
-    """
+def scramble(l, line):
     command, *rest = line.split()
     if command == 'swap':
         """
@@ -100,14 +103,52 @@ def parse(l, line):
             assert False
 
 
+
+def unscramble(l, line):
+    """reverse operations """
+
+    command, *rest = line.split()
+    if command == 'swap':
+        """
+        swap position 7 with position 1
+        swap letter e with letter d
+        """
+        if rest[0] == 'position':
+            return swap_pos(l, int(rest[1]), int(rest[4]))
+        if rest[0] == 'letter':
+            return swap_letter(l, rest[1], rest[4])
+    if command == 'move':
+        """move position 4 to position 0"""
+        assert rest[0] == 'position'
+        return move_pos(l, int(rest[4]), int(rest[1]))
+    if command == 'reverse':
+        """reverse positions 3 through 7"""
+        assert rest[0] == 'positions'
+        return reverse_pos(l, int(rest[1]), int(rest[3]))
+    if command == 'rotate':
+        """rotate right 1 step"""
+        if rest[0] == 'right':
+            return rotate(l, -int(rest[1]))
+        elif rest[0] == 'left':
+            return rotate(l, int(rest[1]))
+        elif rest[0] == 'based':
+            """rotate based on position of letter a"""
+            return reversed_rotate_pos(l, rest[5])
+
     print('lp0 on fire! ', line)
     assert False
 
 
-l = list('abcdefgh')
 with open('input\input21.txt') as f:
     lines = f.readlines()
-    for line in lines:
-        l = parse(l, line)
 
-print(''.join(l))
+l = list('abcdefgh')
+for line in lines:
+    l = scramble(l, line)
+
+print('part A: ', ''.join(l))
+
+l = list('fbgdceah')
+for line in lines[::-1]:
+    l = unscramble(l, line)
+print('part B: ', ''.join(l))
