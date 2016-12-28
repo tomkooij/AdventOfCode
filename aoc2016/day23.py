200c187d5eeeb7611b230bca642d06d51dadc4c1
Tom Kooij <tomkooij@tomkooij.nl>
2016-12-28 14:38:26 +0100

    Day 23: assembunny

--- aoc2016/day23.py
+++ aoc2016/day23.py
@@ -1,70 +1,43 @@
-# adventofcode
+# adventofcode assembunny
 # day 23 2016 -> day 12 2016 -> day 23 2015
 from collections import defaultdict
 
-def int_or_reg(register, regs):
-    try:
-        return int(register)
-    except ValueError:
-        return regs[register]
 
-def evaluate(instruction, regs):
-    opcode, *rest = instruction
-    #print('evaluate ', instruction)
-    if opcode == 'cpy':
-        register = rest[1]
-        value = rest[0]
-        regs[register] = int_or_reg(value, regs)
-        regs['ip'] += 1
-    elif opcode == 'inc':
-        register = rest[0]
-        regs[register] += 1
-        regs['ip'] += 1
-    elif opcode == 'dec':
-        register = rest[0]
-        regs[register] -= 1
-        regs['ip'] += 1
-    elif opcode == 'jnz':
-        register = rest[0]
-        offset = rest[1]
-        zero = int_or_reg(register, regs)
-        if zero:
-            regs['ip'] += int_or_reg(offset, regs)
-        else:
-            regs['ip'] += 1
-    else:
-        assert False, 'unknown instruction.'
+def run(**kwargs):
 
+    def int_or_reg(register):
+        try:
+            return int(register)
+        except ValueError:
+            return regs[register]
 
-def toggle(instruction):
-    opcode, *rest = instruction
-    if opcode == 'inc':
-        opcode = 'dec'
-    elif opcode == 'dec' or opcode == 'tgl':
-        opcode = 'inc'
-    elif opcode == 'jnz':
-        opcode = 'cpy'
-    elif opcode == 'cpy':
-        opcode = 'jnz'
-    return [opcode, *rest]
+    def toggle(instruction):
+        opcode, *rest = instruction
+        if opcode == 'inc':
+            opcode = 'dec'
+        elif opcode == 'dec' or opcode == 'tgl':
+            opcode = 'inc'
+        elif opcode == 'jnz':
+            opcode = 'cpy'
+        elif opcode == 'cpy':
+            opcode = 'jnz'
+        return [opcode, *rest]
 
-assert toggle(['tgl', 'a']) == ['inc', 'a']
-assert toggle(['cpy',  '1',  'a']) == ['jnz', '1', 'a']
+    assert toggle(['tgl', 'a']) == ['inc', 'a']
+    assert toggle(['cpy',  '1',  'a']) == ['jnz', '1', 'a']
 
-def run(*args, **kwargs):
     regs = defaultdict(int)
+    output = []
     for key, value in kwargs.items():
         regs[key] = value
 
+
     while regs['ip'] < len(instructions):
 
-        # hack toggle
         opcode, *rest = instructions[regs['ip']]
+        #print('evaluate: ', opcode, rest)
         if opcode == 'tgl':
-            try:
-                idx = int(rest[0])
-            except ValueError:
-                idx = regs[rest[0]]
+            idx = int_or_reg(rest[0])
             print('toggling ', idx, rest)
             try:
                 instructions[regs['ip']+idx] =\
@@ -72,8 +45,30 @@ def run(*args, **kwargs):
             except IndexError:
                 print('no such index for toggle', idx)
             regs['ip'] += 1
+        elif opcode == 'cpy':
+            register = rest[1]
+            value = rest[0]
+            regs[register] = int_or_reg(value)
+            regs['ip'] += 1
+        elif opcode == 'inc':
+            register = rest[0]
+            regs[register] += 1
+            regs['ip'] += 1
+        elif opcode == 'dec':
+            register = rest[0]
+            regs[register] -= 1
+            regs['ip'] += 1
+        elif opcode == 'jnz':
+            register = rest[0]
+            offset = rest[1]
+            zero = int_or_reg(register)
+            if zero:
+                regs['ip'] += int_or_reg(offset)
+            else:
+                regs['ip'] += 1
+
         else:
-            evaluate(instructions[regs['ip']], regs)
+            assert False, 'unknown instruction.'
 
     return regs['a']
 
@@ -82,6 +77,14 @@ with open('input\input23.txt') as f:
     instructions = [line.rstrip('\n').split() for line in f.readlines()]
     print('part A: ', run(a=7))
 
+"""
+part B:  479007690
+
+real    215m13.299s
+user    215m19.859s
+sys     0m1.318s
+"""
 with open('input\input23.txt') as f:
     instructions = [line.rstrip('\n').split() for line in f.readlines()]
-    #print('part B: ', run(a=12))
+    print('this will take a while...')
+    print('part B: ', run(a=12))
