5be20c73b8eb7735b0181a3c33dc8a6b495eb47b
Tom Kooij <tomkooij@tomkooij.nl>
2017-12-08 15:21:48 +0100

    Day 8

---
+++ aoc2017/day08.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+
+def make_op(reg_name, value):
+    """return a function that modifies a registers"""
+    def op(registers):
+        registers[reg_name] += value
+    return op
+
+
+def parse(row):
+    """parse a row, return a function and a condition to eval()"""
+
+    opreg, op, opval, _if, condreg, condop, condval = row.split() 
+    assert _if == 'if'
+
+    inst = {}
+    val = int(opval)
+    if op == 'dec':
+        val *= -1
+    inst = make_op(opreg, val)
+    
+    condition = 'regs[\''+condreg+'\']'+condop+condval
+    return (inst, condition)
+
+
+with open('input\input08.txt') as f:
+    instructions = [parse(row.strip()) for row in f.readlines()]
+    regs = defaultdict(lambda: 0)
+    max_value = 0
+    for instruction, condition in instructions:
+        if eval(condition):
+            instruction(regs)
+            max_value = max(max_value, max(regs.values()))
+
+    print('part a:', max(regs.values()))
+    print('part b:', max_value)
