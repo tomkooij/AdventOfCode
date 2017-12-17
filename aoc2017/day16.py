976fe0d0d7c3490dee592581589175977d5a877b
Tom Kooij <tomkooij@tomkooij.nl>
2017-12-17 18:57:45 +0100

    Clean-up day 16

--- aoc2017/day16.py
+++ aoc2017/day16.py
@@ -3,7 +3,7 @@ from collections import deque
 
 def solve(instructions, start_position='abcdefghijklmnop', N=1):
     q = deque(start_position)
-    stack = [start_position]
+    cache = [start_position]
 
     for i in range(1, N+1):  
 
@@ -23,14 +23,17 @@ def solve(instructions, start_position='abcdefghijklmnop', N=1):
         new_position = ''.join(q)
         
         if new_position == start_position:
-            return stack[N % i] 
+            #found a duplicate --> sequence will repeat.
+            #period of the sequence is `i` since we count from
+            #start position.  
+            return cache[N % i] 
             
-        stack.append(''.join(q))
+        cache.append(new_position)
     
     return new_position
 
 
 with open('input\input16.txt') as f:
     instructions = f.read().split(',')
-    print('part a:',solve(instructions, N=1))
-    print('part b:',solve(instructions, N=int(1e9)))
+    print('part a:', solve(instructions, N=1))
+    print('part b:', solve(instructions, N=int(1e9)))
