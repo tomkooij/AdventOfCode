ded40cacebad6e173fca3b79e640856417e00aa2
Tom Kooij <tomkooij@tomkooij.nl>
2017-12-20 11:12:11 +0100

    Use better patterns

--- aoc2017/day19.py
+++ aoc2017/day19.py
@@ -1,26 +1,26 @@
 def solve(maze):
     r, c = 0, maze[0].index('|')
     dr, dc = 1, 0
-
+    
+    tile = maze[r][c]
+    assert tile ==  '|'
+    
     letters = ''
     n_steps = 0
 
-    while True:
-        n_steps += 1
-        r, c = r + dr, c + dc
-        tile = maze[r][c]
-        if tile == ' ':
-            break 
+    while tile != ' ':
         if tile.isalpha():
             letters += tile
-        if tile != '+':
-            continue
-        # switch from up/down to left/right or vv
-        dr = 1 - abs(dr)
-        dc = 1 - abs(dc)
-        if maze[r + dr][c + dc] == ' ':
-            # wrong direction, flip:
-            dr, dc = -dr, -dc
+        if tile == '+':
+            # switch from up/down to left/right or vv
+            dr, dc = dc, dr
+            if maze[r + dr][c + dc] == ' ':
+                # wrong direction, flip:
+                dr, dc = -dr, -dc
+        r, c = r + dr, c + dc
+        tile = maze[r][c]
+        n_steps += 1
+
     return letters, n_steps
 
 
